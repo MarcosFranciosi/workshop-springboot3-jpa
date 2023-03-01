3c84a23d1cf7af6eabc14a690cf7739ebf2b3df6
MarcosFranciosi <marcosfranciosi@live.com>
2023-03-01 17:15:28 -0300

    SubTotal & Total Methods

--- src/main/java/com/franciosi/course/entities/Payment.java
+++ src/main/java/com/franciosi/course/entities/Payment.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -25,6 +27,7 @@ public class Payment implements Serializable{
 	
 	@OneToOne
 	@MapsId
+	@JsonIgnore
 	private Order order;
 	
 	public Payment() {
@@ -61,7 +64,7 @@ public class Payment implements Serializable{
 	public void setOrder(Order order) {
 		this.order = order;
 	}
-
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
