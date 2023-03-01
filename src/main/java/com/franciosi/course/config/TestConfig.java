617044c59db6bb2d9300f325365fea0896274e82
MarcosFranciosi <marcosfranciosi@live.com>
2023-03-01 17:02:00 -0300

    Payment, one-to-one association

--- src/main/java/com/franciosi/course/config/TestConfig.java
+++ src/main/java/com/franciosi/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.franciosi.course.entities.Category;
 import com.franciosi.course.entities.Order;
 import com.franciosi.course.entities.OrderItem;
+import com.franciosi.course.entities.Payment;
 import com.franciosi.course.entities.Product;
 import com.franciosi.course.entities.User;
 import com.franciosi.course.entities.enums.OrderStatus;
@@ -82,6 +83,9 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3,oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 	
 	
